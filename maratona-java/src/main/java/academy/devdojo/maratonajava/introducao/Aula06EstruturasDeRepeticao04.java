6bd269929f049428ec4a7fbebb9df6e8dd05769a
Lucas Galvao <lucas.engcomp@outlook.com>
2022-07-29 08:51:33 -0300

    Exercicio parcelamento de carro

---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao04.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao04 {
+
+    /**
+     * Dado o valor de  um carro descubra em quantas vezes ele pode ser parcelado
+     * condição: valorParcelaa >= 1000
+     */
+
+    public static void main(String[] args) {
+        double valorTotal = 50000;
+
+        verificaValorParcela(valorTotal);
+    }
+
+    private static void verificaValorParcela(double valorTotal) {
+        for (int parcela = 1; parcela <= valorTotal; parcela++) {
+            double valorParcela = valorTotal / parcela;
+            if (valorParcela < 1000) {
+                break;
+            }
+            System.out.println("Parcela " + parcela + " R$ " + valorParcela);
+        }
+    }
+}
