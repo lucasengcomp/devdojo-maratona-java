8e2675c3f35a1fad41bff60f49c4631a19e35821
Lucas Galvao <lucas.engcomp@outlook.com>
2022-07-29 07:43:16 -0300

    Estrutura de repetição

---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/introducao/EstruturasDeRepeticao01.java
@@ -0,0 +1,41 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class EstruturasDeRepeticao01 {
+
+
+    public static void main(String[] args) {
+        int count = 0;
+
+        estruturaWhile(count);
+        quebraLinha();
+        quebraLinha();
+        estruturaDoWhile(count);
+        quebraLinha();
+        quebraLinha();
+        lacoFor();
+    }
+
+    private static void quebraLinha() {
+        System.out.println();
+    }
+
+    private static void lacoFor() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Contador dor FOR vale " + i);
+        }
+    }
+
+    private static String estruturaDoWhile(int contador) {
+        do {
+            System.out.println("Dentro do laco do-while pela " + ++contador + "º vez");
+        } while (contador < 10);
+        return "Fim DO-WHILE";
+    }
+
+    private static String estruturaWhile(int contador) {
+        while (contador < 10) {
+            System.out.println(++contador);
+        }
+        return "Fim WHILE";
+    }
+}
