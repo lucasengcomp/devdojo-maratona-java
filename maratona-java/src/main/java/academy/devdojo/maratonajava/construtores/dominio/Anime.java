d06937f9ac8400977e6f98ae2ac9c26a37563a5c
Lucas Galvao <lucas.engcomp@outlook.com>
2022-07-31 14:37:57 -0300

    Sobrecarga de construtores e uso do THIS();

--- maratona-java/src/main/java/academy/devdojo/maratonajava/construtores/dominio/Anime.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/construtores/dominio/Anime.java
@@ -7,14 +7,23 @@ public class Anime {
     private int episodios;
     private String genero;
 
+    private String estudio;
+
     public Anime() {
+        System.out.println("Dentro do construtor sem argumentos");
     }
 
     public Anime(String nome, String tipo, int episodios, String genero) {
+        this();
         this.nome = nome;
         this.tipo = tipo;
         this.episodios = episodios;
         this.genero = genero;
+    }
+
+    public Anime(String nome, String tipo, int episodios, String genero, String estudio) {
+        this(nome, tipo, episodios, genero);
+        this.estudio = estudio;
     }
 
     public void imprime() {
@@ -22,6 +31,7 @@ public class Anime {
         System.out.println(this.tipo);
         System.out.println(this.episodios);
         System.out.println(this.genero);
+        System.out.println(this.estudio);
     }
 
     public String getNome() {
--- maratona-java/src/main/java/academy/devdojo/maratonajava/construtores/test/AnimeTest01.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/construtores/test/AnimeTest01.java
@@ -5,7 +5,7 @@ import academy.devdojo.maratonajava.construtores.dominio.Anime;
 public class AnimeTest01 {
 
     public static void main(String[] args) {
-        Anime anime = new Anime("Akudama drive", "TV", 190, "Ação");
+        Anime anime = new Anime("Akudama drive", "TV", 190, "Ação", "Production XYZ");
         anime.imprime();
     }
 }
