da3e72496f3aa896646980fd64910911aca4e903
Lucas Galvao <lucas.engcomp@outlook.com>
2022-07-31 19:11:34 -0300

    Bloco de inicialização

---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/blocosinicializacao/domain/Anime.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.blocosinicializacao.domain;
+
+public class Anime {
+
+    private String nome;
+    private int[] episodios = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+
+    {
+        System.out.println("Dentro do bloco de inicialização!!!");
+        episodios = new int[100];
+        for (int i = 0; i < episodios.length; i++) {
+            episodios[i] = i + 1;
+        }
+    }
+
+    public Anime() {
+
+        for (int episodio : this.episodios) {
+            System.out.println(episodio + " ");
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+}
