66283ebcdbf03615b0d647f3ef774a289abac973
Lucas Galvao <lucas.engcomp@outlook.com>
2022-08-01 19:01:38 -0300

    Leitura de dados pelo console

---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/associacao/test/LeituraTecladoTest01.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.associacao.test;
+
+import java.util.Scanner;
+
+public class LeituraTecladoTest01 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite seu nome abaixo: ");
+        String nome = entrada.nextLine();
+        System.out.println(nome);
+        System.out.println("Digite sua idade: ");
+        int idade = entrada.nextInt();
+        System.out.println(idade);
+        System.out.println("Digite M ou F para seu sexo: ");
+        char sexo = entrada.next().charAt(0);
+        System.out.println("-------------------");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Sexo: " + sexo);
+    }
+}
