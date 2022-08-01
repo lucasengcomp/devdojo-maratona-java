9072b608171b2cf00aadc7fdc6aed28c86a5403a
Lucas Galvao <lucas.engcomp@outlook.com>
2022-08-01 19:23:43 -0300

    Exercicio leitura do teclado

---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/associacao/test/LeituraTecladoTest02.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.associacao.test;
+
+import java.util.Scanner;
+
+public class LeituraTecladoTest02 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("O grande software do futuro");
+        System.out.println("Digite sua pergunta e eu responderei sim ou não");
+        String pergunta = entrada.nextLine();
+
+        if (pergunta.charAt(0) == ' ') {
+            System.out.println("SIM");
+        } else {
+            System.out.println("NÃO");
+        }
+    }
+}
