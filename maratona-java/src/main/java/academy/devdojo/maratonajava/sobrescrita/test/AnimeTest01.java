57cda76f0b1dfda646a6c40c673e42a846ae6f3e
Lucas Galvao <lucas.engcomp@outlook.com>
2022-08-02 11:14:18 -0300

    Método toString

---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/sobrescrita/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.sobrescrita.test;
+
+import academy.devdojo.maratonajava.sobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Stone");
+        System.out.println(anime);
+    }
+}
