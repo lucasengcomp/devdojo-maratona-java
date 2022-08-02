12fa37221cb9f3e774b900591c4b9920782ae227
Lucas Galvao <lucas.engcomp@outlook.com>
2022-08-02 11:14:39 -0300

    Uso de Constante

---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/modificadorfinal/dominio/Carro.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.modificadorfinal.dominio;
+
+public class Carro {
+
+    private String nome;
+    public static final double VELOCIDADE_LIMITE = 250;
+}
---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/modificadorfinal/test/CarroTest01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.modificadorfinal.test;
+
+import academy.devdojo.maratonajava.modificadorfinal.dominio.Carro;
+
+public class CarroTest01 {
+
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+    }
+}
