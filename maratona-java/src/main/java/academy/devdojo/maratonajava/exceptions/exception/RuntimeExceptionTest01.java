68bff85c7de4d81de76069402b4f7ed07cd0a395
Lucas Galvao <lucas.engcomp@outlook.com>
2022-08-27 20:39:24 -0300

    Propagando uma exceção

--- maratona-java/src/main/java/academy/devdojo/maratonajava/exceptions/exception/RuntimeExceptionTest01.java
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/exceptions/exception/RuntimeExceptionTest01.java
@@ -4,17 +4,18 @@ import java.io.File;
 import java.io.IOException;
 
 public class RuntimeExceptionTest01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         criaNovoArquivo();
     }
 
-    public static void criaNovoArquivo() {
+    public static void criaNovoArquivo() throws IOException {
+        File file = new File("arquivo\\teste.txt");
         try {
-            File file = new File("arquivo\\teste.txt");
             boolean isCriado = file.createNewFile();
             System.out.println("Arquivo criado? " + isCriado);
         } catch (IOException e) {
             e.printStackTrace();
+            throw e;
         }
     }
 }
