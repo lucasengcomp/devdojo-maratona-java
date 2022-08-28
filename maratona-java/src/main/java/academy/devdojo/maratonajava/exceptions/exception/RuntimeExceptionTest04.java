3c032ba69be92eb4181ca2fd522c58c5215b5b29
Lucas Galvao <lucas.engcomp@outlook.com>
2022-08-27 21:28:47 -0300

    Múltiplas exceções

---
+++ maratona-java/src/main/java/academy/devdojo/maratonajava/exceptions/exception/RuntimeExceptionTest04.java
@@ -0,0 +1,23 @@
+package academy.devdojo.maratonajava.exceptions.exception;
+
+public class RuntimeExceptionTest04 {
+    public static void main(String[] args) {
+        excecoes();
+    }
+
+    private static void excecoes() {
+        try {
+            throw new RuntimeException();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Dentro do ArrayIndexOutOfBoundsException");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Dentro do IndexOutOfBoundsException");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Dentro do IllegalArgumentException");
+        } catch (ArithmeticException e) {
+            System.out.println("Dentro do ArithmeticException");
+        } catch (RuntimeException e) {
+            System.out.println("Dentro do RuntimeException");
+        }
+    }
+}
